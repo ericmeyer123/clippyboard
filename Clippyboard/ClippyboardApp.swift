94c143f7b44078a5a30aa507fe83484efa20ac75
eric <emeyer253@gmail.com>
2026-06-22 11:42:37 -0600

    Initial Commit

---
+++ Clippyboard/ClippyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClippyboardApp.swift
+//  Clippyboard
+//
+//  Created by Eric Meyer on 2026-06-22.
+//
+
+import SwiftUI
+
+@main
+struct ClippyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clippyboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clippyboard
+//
+//  Created by Eric Meyer on 2026-06-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
